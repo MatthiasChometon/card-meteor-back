a184e561477bafddcabddb9bc6afc382b63bfddc
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-16 23:37:35 +0100

    feat: delete app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -17,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
