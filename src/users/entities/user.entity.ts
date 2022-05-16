cfb1ceb22c30cfc22d234300f785bd25bd667eb1
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-16 23:41:59 +0100

    feat: connect entity to database

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,14 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
+  @PrimaryGeneratedColumn()
   @Field(() => Int)
   id: number;
 
+  @Column()
   @Field()
   username: string;
 }
