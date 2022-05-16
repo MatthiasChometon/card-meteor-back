b3a3893f6cda3db4711fde056da5810e840565c2
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-16 23:39:22 +0100

    feat: inject envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
