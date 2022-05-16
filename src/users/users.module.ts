bc87738e63748b8d615c535bd4263ba6b7b694c6
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-16 23:42:28 +0100

    feat: use database module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from './users.providers';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  imports: [DatabaseModule],
+  providers: [UsersResolver, UsersService, ...userProviders],
   exports: [UsersService],
 })
 export class UsersModule {}
