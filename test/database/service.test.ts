b4c65181c3f11a129cec39dcd557f5d9653d1061
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-22 17:44:44 +0100

    test: add database service test

---
+++ test/database/service.test.ts
@@ -0,0 +1,31 @@
+import { DatabaseService } from '../../src/database/database.service';
+
+describe('database service', () => {
+  describe('connect', () => {
+    describe('with unvalid port', () => {
+      beforeEach(() => {
+        process.env.DATABASE_PORT = 'unvalid port';
+      });
+
+      it('throws error', async () => {
+        const databaseService = new DatabaseService();
+        await expect(databaseService.connect()).rejects.toThrowError(
+          'unvalid database configuration',
+        );
+      });
+    });
+
+    describe('with unvalid synchronize', () => {
+      beforeEach(() => {
+        process.env.DATABASE_SYNCHRONIZE = 'unvalid synchronize';
+      });
+
+      it('throws error', async () => {
+        const databaseService = new DatabaseService();
+        await expect(databaseService.connect()).rejects.toThrowError(
+          'unvalid database configuration',
+        );
+      });
+    });
+  });
+});
