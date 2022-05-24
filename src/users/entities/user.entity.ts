51c84090f1ee6f0851c77fa1e776759ced348be0
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-24 10:54:34 +0100

    feat: add fields to user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,41 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
 @ObjectType()
 export class User {
   @PrimaryGeneratedColumn()
-  @Field(() => Int)
   id: number;
 
   @Column()
   @Field()
+  last_name: string;
+
+  @Column()
+  @Field()
+  first_name: string;
+
+  @Column({ unique: true })
+  @Field()
   username: string;
+
+  @Column()
+  @Field()
+  password: string;
+
+  @Column()
+  @IsEmail()
+  @Field()
+  email: string;
+
+  @Column()
+  @Field()
+  phone: string;
+
+  @Column({
+    default: '',
+  })
+  @Field()
+  refresh_token: string;
 }
