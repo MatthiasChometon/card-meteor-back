d9e0568b7441784eb563e0c3ebf743fa9ab6b28a
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-25 18:44:16 +0100

    feat: add get function

--- src/products/cards/cards.service.ts
+++ src/products/cards/cards.service.ts
@@ -1,6 +1,10 @@
 import { Injectable, UnauthorizedException, Inject } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { GetCardOrderByInput } from './dto/get-card-orderBy';
+import { GetCardInput } from './dto/get-card.input';
 import { Card } from './entities/card.entity';
+import { CardOrderBy } from './enums/card-orderBy';
+import { PaginationInput } from 'src/database/dto/Pagination.input';
 
 @Injectable()
 export class CardsService {
@@ -8,6 +12,7 @@ export class CardsService {
     @Inject('CARDS_REPOSITORY')
     private cardRepository: Repository<Card>,
   ) {}
+  query: SelectQueryBuilder<Card>;
 
   async save(createCardInput: Partial<Card>): Promise<Card> {
     const cardCreated = await this.cardRepository.save(createCardInput);
@@ -29,15 +34,59 @@ export class CardsService {
     return await this.save(cardUpdated);
   }
 
-  async get(searchName?: string): Promise<Card[]> {
-    let query = this.cardRepository.createQueryBuilder().select();
+  search(key: string, value: string) {
+    this.query = this.query.where(`${key} LIKE :${key}`, {
+      [key]: `%${value}%`,
+    });
+  }
 
-    if (searchName) {
-      query = query.where('LOWER(name) LIKE :name', {
-        name: `%${searchName.toLowerCase()}%`,
-      });
-    }
+  filter(key: string, value: string | number) {
+    const valueToSearch =
+      typeof value === 'string' ? value.toLowerCase() : value.toString();
+    this.query = this.query.where(`${key} = :${key}`, {
+      [key]: `${valueToSearch}`,
+    });
+  }
 
-    return await query.getMany();
+  searchByName(name: string) {
+    this.search('name', name);
+  }
+
+  filterByStep(step: number) {
+    this.filter('step', step);
+  }
+
+  filterByEditor(editor: string) {
+    this.filter('editor', editor);
+  }
+
+  paginate(pagination: PaginationInput) {
+    this.query = this.query.limit(pagination.end).offset(pagination.start);
+  }
+
+  orderBy(orderBy?: GetCardOrderByInput) {
+    const defaultQueryOrderBy: GetCardOrderByInput = {
+      name: CardOrderBy.updateDate,
+      direction: 'ASC',
+    };
+
+    const name = orderBy?.name ?? defaultQueryOrderBy.name;
+    const direction = orderBy?.direction || defaultQueryOrderBy.direction;
+
+    this.query = this.query.orderBy(name, direction);
+  }
+
+  async get(getCardInput: GetCardInput): Promise<Card[]> {
+    const { orderBy, filterBy } = getCardInput;
+    this.query = this.cardRepository.createQueryBuilder().select();
+
+    if (filterBy?.name) this.searchByName(filterBy.name);
+    if (filterBy?.pagination) this.paginate(filterBy.pagination);
+    if (filterBy?.step) this.filterByStep(filterBy.step);
+    if (filterBy?.editor) this.filterByEditor(filterBy.editor);
+
+    this.orderBy(orderBy);
+
+    return await this.query.getMany();
   }
 }
