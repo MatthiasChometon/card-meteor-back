ff5b9c55d790578a0fbd46a1d7374fa17befbcc9
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-25 11:09:14 +0100

    feat: create entity card

---
+++ src/products/cards/entities/card.entity.ts
@@ -0,0 +1,62 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'cards' })
+@ObjectType()
+export class Card {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index({ fulltext: true })
+  @Column()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  level: number;
+
+  @Column()
+  @Field()
+  attribute: string;
+
+  @Column()
+  @Field()
+  attack: number;
+
+  @Column()
+  @Field()
+  defense: number;
+
+  @Column()
+  @Field()
+  description: string;
+
+  @Column()
+  @Field()
+  edition: string;
+
+  @Column()
+  @Field()
+  editor: string;
+
+  @Column()
+  @Field()
+  limitation: number;
+
+  @Column()
+  @Field()
+  coverPicture: string;
+
+  @Column()
+  @Field()
+  backgroundPicture: string;
+
+  @Column({ default: 0.1 })
+  @Field()
+  price: number;
+
+  @Column()
+  @Field()
+  state: number;
+}
