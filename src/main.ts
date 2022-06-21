cd54f4b91eb5f6f5101f9739c58c1549f2ccab91
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:29:51 +0100

    feat: change to express to use graphqlUploadExpress

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { graphqlUploadExpress } from 'graphql-upload';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(graphqlUploadExpress());
+  app.useStaticAssets(join(__dirname, 'uploads'));
   await app.listen(3000);
 }
 bootstrap();
