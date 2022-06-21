4356075d90b6480c1158827033be9f58b480438e
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:27:54 +0100

    feat: add upload pictures to card creation

--- src/products/cards/cards.service.ts
+++ src/products/cards/cards.service.ts
@@ -1,22 +1,62 @@
 import { Injectable, UnauthorizedException, Inject } from '@nestjs/common';
-import { Repository, SelectQueryBuilder } from 'typeorm';
-import { GetCardOrderByInput } from './dto/get-card-orderBy';
-import { GetCardInput } from './dto/get-card.input';
+import { UploadService } from '../../upload/upload.service';
+import { Repository } from 'typeorm';
 import { Card } from './entities/card.entity';
-import { CardOrderBy } from './enums/card-orderBy';
-import { PaginationInput } from 'src/database/dto/Pagination.input';
+import { CreateCardInput } from './dto/create-card.input';
+import { FileUpload } from 'graphql-upload';
 
 @Injectable()
 export class CardsService {
   constructor(
     @Inject('CARDS_REPOSITORY')
     private cardRepository: Repository<Card>,
+    private uploadService: UploadService,
   ) {}
-  query: SelectQueryBuilder<Card>;
+  backgroundPicture: FileUpload;
+  coverPicture: FileUpload;
+  createCardInput: CreateCardInput;
+  newCard: Partial<Card>;
 
-  async save(createCardInput: Partial<Card>): Promise<Card> {
-    const cardCreated = await this.cardRepository.save(createCardInput);
+  async create(
+    createCardInput: CreateCardInput,
+    coverPicture: FileUpload,
+    backgroundPicture: FileUpload,
+  ): Promise<Card> {
+    this.createCardInput = createCardInput;
+    this.buildPictures(backgroundPicture, coverPicture);
+    const [cardCreated] = await Promise.all([
+      this.cardRepository.save(this.newCard),
+      this.uploadPictures(),
+    ]);
     return cardCreated;
+  }
+
+  buildPictures(backgroundPicture: FileUpload, coverPicture: FileUpload) {
+    this.backgroundPicture = backgroundPicture;
+    this.coverPicture = coverPicture;
+    this.buildPicturesName();
+    this.newCard = {
+      ...this.createCardInput,
+      backgroundPicture: this.backgroundPicture.filename,
+      coverPicture: this.coverPicture.filename,
+    };
+  }
+
+  private buildPicturesName() {
+    const backgroundPictureExtension = this.uploadService.getFileExtension(
+      this.backgroundPicture,
+    );
+    this.backgroundPicture.filename = this.uploadService.generateFileName(
+      backgroundPictureExtension,
+    );
+    this.coverPicture.filename = this.uploadService.generateFileName('svg');
+  }
+
+  private async uploadPictures() {
+    await Promise.all([
+      this.uploadService.uploadFile(this.backgroundPicture, 'background'),
+      this.uploadService.uploadFile(this.coverPicture, 'cover'),
+    ]);
   }
 
   async findOne(cardInformations: Partial<Card>): Promise<Card> {
