bb7783fc886df511c98f3be935c95e5b78a6492a
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:32:24 +0100

    feat: add CardsListService and UploadService to cards module

--- src/products/cards/cards.module.ts
+++ src/products/cards/cards.module.ts
@@ -3,9 +3,17 @@ import { CardsService } from './cards.service';
 import { CardsResolver } from './cards.resolver';
 import { cardProviders } from './cards.providers';
 import { DatabaseModule } from '../../database/database.module';
+import { CardsListService } from './cardsList.service';
+import { UploadService } from '../../upload/upload.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [CardsResolver, CardsService, ...cardProviders],
+  providers: [
+    CardsResolver,
+    CardsService,
+    ...cardProviders,
+    CardsListService,
+    UploadService,
+  ],
 })
 export class CardsModule {}
