66f6ce27d7dc7b935b9f9a13ed125c9991861170
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:28:47 +0100

    feat: add upload picture to cards resolvers

--- src/products/cards/cards.resolver.ts
+++ src/products/cards/cards.resolver.ts
@@ -2,6 +2,8 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { CardsService } from './cards.service';
 import { Card } from './entities/card.entity';
 import { CreateCardInput } from './dto/create-card.input';
+import { GetCardInput } from './dto/get-card.input';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { UpdateCardInput } from './dto/update-card.input';
 import { GetCardInput } from './dto/get-card.input';
 
@@ -12,8 +14,16 @@ export class CardsResolver {
   @Mutation(() => Card)
   createCard(
     @Args('createCardInput') createCardInput: CreateCardInput,
+    @Args({ name: 'coverPicture', type: () => GraphQLUpload })
+    coverPicture: FileUpload,
+    @Args({ name: 'backgroundPicture', type: () => GraphQLUpload })
+    backgroundPicture: FileUpload,
   ): Promise<Card> {
-    return this.cardsService.save(createCardInput);
+    return this.cardsService.create(
+      createCardInput,
+      coverPicture,
+      backgroundPicture,
+    );
   }
 
   @Mutation(() => Card)
