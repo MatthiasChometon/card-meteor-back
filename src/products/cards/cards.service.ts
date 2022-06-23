2f30f866ac49a4075f86201beb4ade3f30e150e6
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-23 19:54:47 +0100

    feat: use cardsPicturesService in cardsService

--- src/products/cards/cards.service.ts
+++ src/products/cards/cards.service.ts
@@ -1,76 +1,79 @@
-import { Injectable, UnauthorizedException, Inject } from '@nestjs/common';
-import { UploadService } from '../../upload/upload.service';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Card } from './entities/card.entity';
 import { CreateCardInput } from './dto/create-card.input';
 import { FileUpload } from 'graphql-upload';
+import { CardsPicturesService } from './cardsPictures.service';
 
 @Injectable()
 export class CardsService {
   constructor(
     @Inject('CARDS_REPOSITORY')
     private cardRepository: Repository<Card>,
-    private uploadService: UploadService,
+    private cardsPicturesService: CardsPicturesService,
   ) {}
-  backgroundPicture: FileUpload;
-  coverPicture: FileUpload;
-  createCardInput: CreateCardInput;
-  newCard: Partial<Card>;
+  card: Card;
 
   async create(
     createCardInput: CreateCardInput,
     coverPicture: FileUpload,
     backgroundPicture: FileUpload,
+    editor: string,
   ): Promise<Card> {
-    this.createCardInput = createCardInput;
-    this.buildPictures(backgroundPicture, coverPicture);
+    this.cardsPicturesService.backgroundPicture = backgroundPicture;
+    this.cardsPicturesService.coverPicture = coverPicture;
+    this.cardsPicturesService.buildPicturesName();
+
+    const newCard = {
+      ...createCardInput,
+      editor,
+      backgroundPicture: this.cardsPicturesService.backgroundPicture.filename,
+      coverPicture: this.cardsPicturesService.coverPicture.filename,
+    };
     const [cardCreated] = await Promise.all([
-      this.cardRepository.save(this.newCard),
-      this.uploadPictures(),
+      this.cardRepository.save(newCard),
+      this.cardsPicturesService.uploadPictures(),
     ]);
     return cardCreated;
   }
 
-  buildPictures(backgroundPicture: FileUpload, coverPicture: FileUpload) {
-    this.backgroundPicture = backgroundPicture;
-    this.coverPicture = coverPicture;
-    this.buildPicturesName();
-    this.newCard = {
-      ...this.createCardInput,
-      backgroundPicture: this.backgroundPicture.filename,
-      coverPicture: this.coverPicture.filename,
-    };
-  }
-
-  private buildPicturesName() {
-    const backgroundPictureExtension = this.uploadService.getFileExtension(
-      this.backgroundPicture,
-    );
-    this.backgroundPicture.filename = this.uploadService.generateFileName(
-      backgroundPictureExtension,
-    );
-    this.coverPicture.filename = this.uploadService.generateFileName('svg');
-  }
-
-  private async uploadPictures() {
-    await Promise.all([
-      this.uploadService.uploadFile(this.backgroundPicture, 'background'),
-      this.uploadService.uploadFile(this.coverPicture, 'cover'),
-    ]);
-  }
-
-  async findOne(cardInformations: Partial<Card>): Promise<Card> {
+  async findOne(cardInformations: Partial<Card>): Promise<Card | undefined> {
     return await this.cardRepository.findOne({
       where: { ...cardInformations },
     });
   }
 
-  async updateOne(id: number, payload: Partial<Card>): Promise<Card> {
-    const card = await this.findOne({ id });
+  async findOneOrError(cardInformations: Partial<Card>): Promise<Card> {
+    const card = await this.findOne(cardInformations);
+    if (card === undefined) throw new NotFoundException();
+    return card;
+  }
 
-    if (!card) throw new UnauthorizedException('Card not exist');
+  async updateOne(
+    cardInformations: Partial<Card>,
+    payload: Partial<Card>,
+    coverPicture: FileUpload,
+    backgroundPicture?: FileUpload,
+  ): Promise<Card> {
+    this.card = await this.findOneOrError(cardInformations);
+    this.cardsPicturesService.coverPicture = coverPicture;
+    this.cardsPicturesService.coverPicture.filename = this.card.coverPicture;
 
-    const cardUpdated = { ...card, ...payload };
-    return await this.cardRepository.save(cardUpdated);
+    const cardToUpdate = { ...this.card, ...payload };
+    const [cardUpdated] = await Promise.all([
+      this.cardRepository.save(cardToUpdate),
+      this.uploadBackgroundPicture(backgroundPicture),
+      this.cardsPicturesService.uploadCoverPicture(),
+    ]);
+    return cardUpdated;
+  }
+
+  private async uploadBackgroundPicture(backgroundPicture: FileUpload) {
+    if (backgroundPicture !== undefined) {
+      this.cardsPicturesService.backgroundPicture = backgroundPicture;
+      this.cardsPicturesService.backgroundPicture.filename =
+        this.card.backgroundPicture;
+      await this.cardsPicturesService.uploadBackgroundPicture();
+    }
   }
 }
