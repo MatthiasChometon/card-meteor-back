130dad25cdab7313be2adb9128ed156edc432d1d
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-23 19:50:26 +0100

    feat: add CardsPicturesService to card module

--- src/products/cards/cards.module.ts
+++ src/products/cards/cards.module.ts
@@ -5,11 +5,13 @@ import { cardProviders } from './cards.providers';
 import { DatabaseModule } from '../../database/database.module';
 import { CardsListService } from './cardsList.service';
 import { UploadService } from '../../upload/upload.service';
+import { CardsPicturesService } from './cardsPictures.service';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
     CardsResolver,
+    CardsPicturesService,
     CardsService,
     ...cardProviders,
     CardsListService,
