7b0aca3c012ae4278529948e7e46d8ba3c76d268
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-25 23:57:19 +0100

    feat: initialize firebase in main

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as admin from 'firebase-admin';
+import { firebaseConfig } from '../firebase';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig),
+  });
   app.use(graphqlUploadExpress());
   app.useStaticAssets(join(__dirname, 'uploads'));
   await app.listen(3000);
