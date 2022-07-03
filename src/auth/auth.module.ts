ad4cefde4072e5fbe33549e5e8d7195a22f3678c
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-03 17:32:57 +0200

    feat: add env to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,16 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt-strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
     UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.production', '.env.local', '.env'],
+      isGlobal: true,
+    }),
     JwtModule.register({
       signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION_TIME },
       secret: process.env.ACCESS_TOKEN_SECRET,
